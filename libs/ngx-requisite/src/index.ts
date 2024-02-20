d2df45e1b54c0b4e108aed475f74b33aa9a02862
Chandu <chandru9052@gmail.com>
2024-02-20 16:37:20 +0530

    [chandu] added references

--- libs/ngx-requisite/src/index.ts
+++ libs/ngx-requisite/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/ngx-requisite.module';
 export * from './lib/interfaces';
 export * from './lib/decorators/index';
+export * from './lib/pipes/safeHtml';
+export * from './lib/pipes/safeUrl';
